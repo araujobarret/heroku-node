768fc59487c39a234aa9bde055bc622e8e9a6add
Carlos Alberto de Araujo Barreto <tiamatpontal@gmail.com>
2017-08-17 17:58:36 -0300

    Heroku integration

---
+++ node-server/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err)
+			console.log('Unable to append to the file');
+	});
+
+	next();
+});
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () =>{
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		title: 'Home Page',		
+		message: 'Hbs templating'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		title: 'About Page'		
+	});
+});
+
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		title: 'Projects'		
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		error: 'Error in the request'
+	});
+});
+
+app.listen(port, () => {
+	console.log('Serving listening at port 3000');
+});
